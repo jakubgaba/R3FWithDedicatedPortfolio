14469e4c50abcb4f895844bde9669aad8085dff9
Jakub <j.gaba@email.cz>
2024-01-16 18:06:31 +0100

    End

--- src/index.jsx
+++ src/index.jsx
@@ -7,12 +7,13 @@ const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
-        camera={ {
+        className='r3f'
+        camera={{
             fov: 45,
             near: 0.1,
             far: 2000,
-            position: [ -3, 1.5, 4 ]
-        } }
+            position: [-3, 1.5, 4]
+        }}
     >
         <Experience />
     </Canvas>

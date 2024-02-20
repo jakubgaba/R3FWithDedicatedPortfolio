56f8bb5590ad94e0688fd74627184962bb639518
Jakub <j.gaba@email.cz>
2024-02-20 19:50:05 +0100

    New

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,62 +1,47 @@
-import { Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF } from '@react-three/drei'
+import { Text, Html, ContactShadows, PresentationControls, Float, useGLTF } from '@react-three/drei'
+import { useThree } from '@react-three/fiber'
+
 
 export default function Experience() {
+    const { size } = useThree();
+    const computer = useGLTF('/model.gltf')
 
-    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+    const config = {
+        large: {
+            presentationControls: { rotation: [0.2, -Math.PI * 2.1, 0.1], polar: [0, Math.PI / 3], azimuth: [-Math.PI / 10, Math.PI / 3], snap: true, zoom: 0.6 },
+            rectAreaLight: { position: [0, 0.55, -1.15], rotation: [-0.1, Math.PI, 0] },
+            primitive: { positionY: -1.2 },
+            text: { position: [1.5, 0.5, 0.25], rotationY: -1.4 },
+        },
+        small: {
+            presentationControls: { rotation: [0.2, -Math.PI * 2.2, 0.2], polar: [0, Math.PI / 3], azimuth: [-Math.PI / 10, Math.PI / 3], snap: true, zoom: 0.6 },
+            rectAreaLight: { position: [0, 0.55, -2.4], rotation: [-0.1, Math.PI, 0] },
+            primitive: { positionY: -1.5, positionZ: -1 },
+            text: { position: [1.6, 0.5, -1], rotationY: -1.4, }
+        }
+    };
 
-    return <>
-        <Environment preset='city' />
-        <color args={['#241a1a']} attach={'background'} />
+    // Function to select configuration
+    const sceneConfig = size.width > 500 ? config.large : config.small;
 
-        <PresentationControls
-            global
-            rotation={[0.13, 0.1, 0]}
-            polar={[-0.4, 0.2]}
-            azimuth={[-1, 0.75]}
-            config={{ mass: 2, tension: 400 }}
-            snap={{ mass: 4, tension: 400 }}
-        >
-            <Float rotationIntensity={0.4}>
-
-                <rectAreaLight
-                    width={2.5}
-                    height={1.65}
-                    intensity={65}
-                    color={'#ffffff'}
-                    rotation={[-0.1, Math.PI, 0]}
-                    position={[0, 0.55, -1.15]}
-                />
-
-                <primitive
-                    object={computer.scene}
-                    position-y={-1.2}
-                >
-                    <Html
-                        transform
-                        wrapperClass='htmlScreen'
-                        distanceFactor={1.17}
-                        position={[0, 1.56, -1.4]}
-                        rotation-x={-0.256}
-                       
-                    >
-                        <iframe src="https://www.iron-shop.cc" />
+    // Scene components
+    const SceneComponent = ({ config }) => (
+        <PresentationControls {...config.presentationControls}>
+            <Float rotationIntensity={0.3}>
+                <rectAreaLight width={2.5} height={1.65} intensity={25} color={'#ffffff'} {...config.rectAreaLight} />
+                <primitive object={computer.scene} position-y={config.primitive.positionY} position-z={config.primitive.positionZ || 0}>
+                    <Html transform wrapperClass='htmlScreen' distanceFactor={1.17} position={[0, 1.56, -1.4]} rotation-x={-0.256}>
+                        <iframe src="https://jakubgabaportfolio.vercel.app/" title="Embedded Page"></iframe>
                     </Html>
                 </primitive>
-                <Text
-                    font='./bangers-v20-latin-regular.woff'
-                    fontSize={1}
-                    position={[2, 0.75, 0.75]}
-                    rotation-y={-1.25}
-                    children={'JAKUB\nGABA'}
-                    textAlign='left'
-                ></Text>
+                <Text font='./bangers-v20-latin-regular.woff' fontSize={1} position={config.text.position} rotation-y={config.text.rotationY} children={'JAKUB\nGABA'} textAlign='left'></Text>
             </Float>
         </PresentationControls>
-        <ContactShadows
-            position-y={-1.4}
-            opacity={0.4}
-            scale={5}
-            blur={2.4}
-        />
+    );
+
+    return <>
+        <color args={['#241a1a']} attach={'background'} />
+        <SceneComponent config={sceneConfig} />
+        <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
     </>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,67 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import { StrictMode, Suspense } from 'react'
+import { NoToneMapping, SRGBColorSpace } from 'three'
+import { Loader } from '@react-three/drei'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
+// Define styles
+const containerStyles = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.9)',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: 10,
+};
+
+const innerStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+};
+const dataStyles = {
+    marginTop: '10%',
+    color: '#fff',
+    fontFamily: 'Arial, sans-serif',
+    fontSize: '25px',
+    backgroundColor: 'black'
+};
+
 root.render(
-    <Canvas
-        className='r3f'
-        camera={{
-            fov: 45,
-            near: 0.1,
-            far: 2000,
-            position: [-3, 1.5, 4]
-        }}
-    >
-        <Experience />
-    </Canvas>
+    <StrictMode>
+        <Canvas flat dpr={[1, 2]}
+            gl={{
+                antialias: true,
+                toneMapping: NoToneMapping,
+                outputColorSpace: SRGBColorSpace,
+                
+            }}
+            className='r3f'
+            camera={{
+                fov: 40,
+                near: 0.1,
+                far: 2000,
+                position: [-3, 1.5, 4]
+            }}
+        >
+            <Suspense fallback={null}>
+                <Experience />
+            </Suspense>
+        </Canvas>
+        <Loader
+            containerStyles={{ ...containerStyles }} // Spread the container styles
+            innerStyles={{ ...innerStyles }} // Spread the inner container styles
+            dataStyles={{ ...dataStyles }} // Spread the text styles
+            dataInterpolation={(p) => `Loading portfolio ${p.toFixed(2)}%`} // Customize loading text
+            initialState={(active) => active} // Use loader's active state
+        />
+    </StrictMode>
 )
